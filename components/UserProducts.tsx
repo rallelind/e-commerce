aede1ff12c147209d752adf2492040f8c0eb4cbd
Rasmus Lind <rasmus1234555@hotmail.dk>
2022-04-09 17:00:27 +0200

    made api endpoint to show products a user has uploaded

--- components/UserProducts.tsx
+++ components/UserProducts.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import { Grid, Loading, Container } from "@nextui-org/react";
+import ProductCard from './ProductCard'
+import { useSession  } from 'next-auth/react';
+import { useState, useEffect } from 'react';
 
-const UserProducts = () => {
+
+
+const UserProducts: React.FC = (props) => {
+
+    const { data: session } = useSession();
+
+    const [userProduct, setUserProduct] = useState([])
+
+    useEffect(() => {
+        fetch("api/product/user-products")
+        .then(res => res.json())
+        .then(data => setUserProduct(data))
+    }, [])
+
+    console.log(userProduct)
+
+    if(!session) {
+        return (
+            <h1>You need to be authenticated</h1>
+        )
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Grid xs={5}>
+            <Grid.Container gap={4} justify="center">
+                {userProduct.length === 0 &&
+                        <Loading size="xl" />
+                }
+                  
+                {userProduct.map((post) => (
+                    <ProductCard key={post.id} post={post} />
+                ))}
+                
+            </Grid.Container>
+        </Grid>
     )
 }
 
---
+++ pages/api/product/user-products.ts
@@ -0,0 +1,18 @@
+import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getSession({ req })
+    const userProducts= await prisma.post.findMany({
+        where: {
+            author: { email: session.user.email }
+        },
+        include: {
+            author: {
+                select: { name: true }
+            },
+        },
+    });
+    res.json(userProducts)
+}
