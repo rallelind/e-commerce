78218550db76272df27ba8fab3e48c1a382a4467
Rasmus Lind <rasmus1234555@hotmail.dk>
2022-05-18 17:35:14 +0200

    reverted back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { Grid } from '@nextui-org/react'
 import ProductCard, { PostProps } from '../components/utils/ProductCard'
-import { GetStaticProps } from "next"
+import { GetServerSideProps } from "next"
 import { useRouter } from 'next/router'
 import prisma from "../lib/prisma"
 import Layout from '../components/utils/Layout';
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
     include: {
