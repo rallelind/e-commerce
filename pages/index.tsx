7898ee728c627e0d8daeb2759d85d2d0ce9bf36e
Rasmus Lind <rasmus1234555@hotmail.dk>
2022-05-18 17:33:01 +0200

    tried something

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { Grid } from '@nextui-org/react'
 import ProductCard, { PostProps } from '../components/utils/ProductCard'
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import { useRouter } from 'next/router'
 import prisma from "../lib/prisma"
 import Layout from '../components/utils/Layout';
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
     include: {
