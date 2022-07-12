65a326ae4001d0ac246310f3ea77d9d756561e61
Rasmus Lind <rasmus1234555@hotmail.dk>
2022-07-12 21:06:28 +0200

    added check for stripeconnect

--- pages/profile/upload-product.tsx
+++ pages/profile/upload-product.tsx
@@ -1,11 +1,27 @@
 import UserAppShell from "../../components/profilePage/appShell/UserAppShell";
 import UploadProduct from "../../components/profilePage/uploadProduct/UploadProduct";
+import prisma from "../../lib/prisma";
+import { getSession } from "next-auth/react";
 
-export default function UploadProducts() {
+export const getServerSideProps = async ({ req }) => {
+
+    const session = await getSession({ req })
+
+    const user = await prisma.user.findUnique({
+        where: { email: session?.user?.email },
+        select: { stripeConnect: true }
+    })
+
+    return {
+        props: { user }
+    }
+}
+
+export default function UploadProducts({ user }) {
 
     return (
         <UserAppShell inbox={false} navbar={null}>
-            <UploadProduct stripeConnect={true} />
+            <UploadProduct stripeConnect={user.stripeConnect} />
         </UserAppShell>
     )
 }
