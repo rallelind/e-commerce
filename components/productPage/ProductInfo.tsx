bfdea960867e41162cda6114651ba0aeffd5aef6
Rasmus Lind <rasmus1234555@hotmail.dk>
2022-07-13 20:10:16 +0200

    another try

--- components/productPage/ProductInfo.tsx
+++ components/productPage/ProductInfo.tsx
@@ -72,7 +72,7 @@ const ProductInfo: React.FC<ProductInfoProps> = ({ title, description, avatar, m
                         disableOutsideEvents={true}
                         minDate={new Date(minDate) > new Date() ? new Date(minDate) : new Date()}
                         maxDate={new Date(maxDate)}
-                        excludeDate={(date) => bookedDates.some((dates) => date === new Date(dates))}
+                        excludeDate={(date) => bookedDates.some((dates) => date.getTime() === new Date(dates).getTime())}
                     />
                 </Container>
             </Container>
